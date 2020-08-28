acef698534e89d539d9782148c45d85598fab356
mccartney0 <traduvert@icloud.com>
2020-08-27 21:49:03 -0300

    Image Compressor

---
+++ app.js
@@ -0,0 +1,22 @@
+const compress_images = require('compress-images');
+
+const pathInput = "./src/*";
+
+function compress(pathInput, outputPath) {
+
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+        }
+    );
+
+}
+compress(pathInput, "./compressed/");
